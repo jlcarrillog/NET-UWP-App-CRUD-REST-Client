a197d3830375b1101ef3f09614f5315a1eadfe7b
Sistemas Integrales en Computación y Electrónica <sice_support@hotmail.com>
2019-03-05 12:46:08 -0600

    Start MainPage

--- App/App/Views/MainPage.xaml.cs
+++ App/App/Views/MainPage.xaml.cs
@@ -1,37 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Core;
-using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace App
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class Layout : Page
+    public sealed partial class MainPage : Page
     {
         public string Title;
-        public Layout()
+        public MainPage()
         {
             this.InitializeComponent();
-            Navigation.Header = "Inicio" ;
-            rootFrame.Navigate(typeof(MainPage));
+            Navigation.Header = "Inicio";
         }
-
         private void RootFrame_NavigationFailed(object sender, NavigationFailedEventArgs e)
         {
             throw new Exception("Error: " + e.SourcePageType.FullName);
@@ -55,7 +35,7 @@ namespace App
                         rootFrame.Navigate(typeof(Empleados));
                         break;
                     default:
-                        rootFrame.Navigate(typeof(MainPage));
+                        this.Frame.Navigate(typeof(MainPage));
                         break;
 
                 }
